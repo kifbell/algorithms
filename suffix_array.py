1ccb44a879a51eda404d838111b60e3c764f2886
Kirill Belyakov <dog.kirill.belyakov@gmail.com>
2021-12-12 16:35:10 +0300

    feat: find patters, input + uotput

--- suffix_array.py
+++ suffix_array.py
@@ -1,4 +1,6 @@
 import operator as op
+from typing import List
+
 from math import log2
 
 
@@ -38,10 +40,6 @@ def assign_symbols_by_ind(text: str, arr: list[Symbols]):
         symbol.value = text[symbol.ind: symbol.ind + 2 * len(symbol.value)]
 
 
-def sort_extend_ec_reset_ec():
-    pass
-
-
 def upgrade_ec(arr: list[Symbols], ec_store):
     for symbol in arr:
         symbol.ec = ec_store[symbol.value[:len(symbol.value) // 2]] + str(symbol.ec)
@@ -72,38 +70,60 @@ def reset_ec(arr: list[Symbols], ec_store):
     return ec_store
 
 
-def main_func(text: str):
+def get_suffix_array(text: str):
     get_letter = op.attrgetter("value")
-    text = 'abacababacaba'
+    text = 'abcdabc'
 
     extended_text = extended_string(text)
     main_arr = [Symbols(letter, ind) for ind, letter in enumerate(extended_text)]
 
     main_arr.sort(key=lambda sym: get_letter(sym))
-    ec_dict = initiate_ec(main_arr)
-    ec_dict = reset_ec(main_arr, ec_dict)
+    ec_storage = initiate_ec(main_arr)
+    ec_storage = reset_ec(main_arr, ec_storage)
 
-    print(extended_text)
+    # print(extended_text)
     for _ in range(int(log2(len(extended_text)))):
         reduce_indices(main_arr)
         assign_symbols_by_ind(extended_text, main_arr)
         main_arr.sort(key=lambda sym: get_letter(sym))
-        upgrade_ec(main_arr, ec_dict)
-        ec_dict = reset_ec(main_arr, ec_dict)
-        for symbol in main_arr:
-            print(f"{symbol.value}: ec: {symbol.ec}, start ind: {symbol.ind}")
-        print()
+        upgrade_ec(main_arr, ec_storage)
+        ec_storage = reset_ec(main_arr, ec_storage)
+
+        # for symbol in main_arr:
+        #     print(f"{symbol.value}: ec: {symbol.ec}, start ind: {symbol.ind}")
+        # print()
+
+    return main_arr
+    # print(ec_storage)
 
 
 def input_func(file_name):
-    pass
     with open(file_name, "r") as stream:
-        text = stream.readline()
-    return text
+        text = stream.readline().strip()
+        return text, [pattern_line.strip() for pattern_line in stream.readlines()]
 
 
-def main():
-    pass
+def find_patterns(suffix_array: list[Symbols], patterns: list[str]):
+    ans = list()
+    for i, pattern in enumerate(patterns):
+        this_pattern = list()
+        for symbol in suffix_array:
+            if symbol.value.startswith(pattern):
+                this_pattern.append(symbol.ind)
+        ans.append([i + 1, pattern, sorted(this_pattern)])
+
+    return ans
 
 
-main_func('asdf')
+def output_patterns(patterns: list):
+    upgraded_str = lambda index: str(index + 1)
+    for pattern_nubmer, _, start_indices in patterns:
+        if start_indices:
+            print(f"{pattern_nubmer}: {','.join(list(map(upgraded_str, start_indices)))}")
+
+
+if __name__ == "__main__":
+    text, patterns = input_func("input.txt")
+    suffix_array = get_suffix_array(text)
+    ans = find_patterns(suffix_array, patterns)
+    output_patterns(ans)
